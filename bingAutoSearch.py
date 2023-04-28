4190bae09c70fbb17e4ad6193c238d9e1f05fdbd
Khush Jhalani <khushjhalani.03@gmail.com>
2023-04-29 03:32:15 +0530

    Added exception handling and search record and synced both the codes

--- bingAutoSearch.py
+++ bingAutoSearch.py
@@ -2,10 +2,11 @@ import random
 import json,requests
 from time                                           import sleep
 from selenium                                       import webdriver
-# from selenium.webdriver.edge.options                import Options
-# from selenium.webdriver.edge.service                import Service
+from selenium.webdriver.edge.options                import Options
 import argparse as ap
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='bingAutoSearch.log', filemode='w')
 
 # #TO OPEN MY DEFAULT EDGE PROFILE AND NOT OPEN A NEW BOT PROFILE
 # edge_driver_path = "C:\Program Files (x86)\edgedriver_win64"
@@ -17,9 +18,8 @@ import argparse as ap
 def get_arguments():
     parser = ap.ArgumentParser()
     parser.add_argument('-s', '--searches', help='No of searches', required=True, type=int)
+    parser.add_argument('-u', '--username', help=r"Username of your system profile. Can be found in C:\Users\[USERNAME]", required=True, type=str)
     inputs = parser.parse_args()
-    # if not inputs.example:
-    #    parser.error("[-] Please specify an input")
     return inputs
 
 response = requests.get("https://www.randomlists.com/data/words.json")
@@ -30,26 +30,49 @@ def randomText():
     # return f"{base_url}{random.choice(wLsample)}+{random.choice(wLsample)}+{random.choice(wLsample)}"
     return base_url + random.choice(wLsample)
 
-
-# def waitForPageLoad():
-# while driver.execute_script("return document.readyState") != ("complete" or 'interactive'):
-#     sleep(0.5)
-
-def randomBingSearch(times: int = 10):
+def randomBingSearch(driver, times: int = 10):
+    '''This function will search random words on bing.com and returns the number of searches done'''
+    count = 0
     for i in range(0, times):
-        driver.get(randomText())
-        sleep(1)
-        # waitForPageLoad()
+        try:
+            driver.get(randomText())
+            sleep(1)
+            if 'Search' in driver.title:
+                count += 1
+        except Exception as e:
+            logging.error(f"\n\n\n\n\nError during {count+1} search: \t {e}")
+            print(f"Error during {count+1} search. Continuing...")
+    driver.quit()
+    return count 
+
+def get_driver(uname):
+    edge_options = Options()
+    edge_options.add_argument(f"user-data-dir=C:\\Users\\{uname}\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default")
+    driver = webdriver.Edge(options=edge_options)
+    return driver
 
 
 #Driver Code
-if __name__ == "__main__":
-    inp = get_arguments()
-    driver = webdriver.Edge()
-    driver.get("https://rewards.bing.com/")
-    sleep(3)
-    #Open a new tab
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    randomBingSearch(inp.searches)
-    driver.close()
+if __name__=="__main__":
+    inp = get_arguments()    
+    maxTries = 5
+    searchesDone = 0
+    while searchesDone<inp.searches:
+        if maxTries == 0:
+            print("Maximum tries reached. Exiting...")
+            exit()
+        try:
+            driver = get_driver(inp.username)
+        except Exception as e:
+            logging.error(f"Error in getting driver: \t {e}")
+            print(f"You might have provided wrong username.PLEASE CHECK USERNAME. Error in webdriver.")
+            exit()
+        driver.get("https://rewards.bing.com/")
+        sleep(3)
+        
+        # driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[-1])
+        searchesDone += randomBingSearch(driver, inp.searches-searchesDone)
+        driver.quit()
+        maxTries -= 1
+        print(f"Searches done: {searchesDone}")
--- bingMobileSearch.py
+++ bingMobileSearch.py
@@ -4,14 +4,15 @@ from time                                           import sleep
 from selenium                                       import webdriver
 from selenium.webdriver.edge.options                import Options
 import argparse as ap
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',filename='bingMobileSearch.log', filemode='w')
 
 def get_arguments():
     parser = ap.ArgumentParser()
     parser.add_argument('-s', '--searches', help='No of searches', required=True, type=int)
     parser.add_argument('-u', '--username', help=r"Username of your system profile. Can be found in C:\Users\[USERNAME]", required=True, type=str)
     inputs = parser.parse_args()
-    # if not inputs.example:
-    #    parser.error("[-] Please specify an input")
     return inputs
 
 response = requests.get("https://www.randomlists.com/data/words.json")
@@ -22,10 +23,19 @@ def randomText():
     return base_url + random.choice(wLsample)
 
 def randomBingSearch(driver, times: int = 10):
+    '''This function will search random words on bing.com and returns the number of searches done'''
+    count = 0
     for i in range(0, times):
-        driver.get(randomText())
-        sleep(1)
-        # waitForPageLoad()
+        try:
+            driver.get(randomText())
+            sleep(1)
+            if 'Search' in driver.title:
+                count += 1
+        except Exception as e:
+            logging.error(f"\n\n\n\n\nError during {count+1} search: \t {e}")
+            print(f"Error during {count+1} search. Continuing...")
+    driver.quit()
+    return count        
 
 def get_driver(uname):
     mobile_emulation = {
@@ -38,12 +48,31 @@ def get_driver(uname):
     driver = webdriver.Edge(options=edge_options)
     return driver
 
+# def waitForPageLoad(driver):
+#     while driver.execute_script("return document.readyState") not in ['complete','interactive']:
+#         sleep(0.5)
+
 #Driver Code
 if __name__=="__main__":
-    inp = get_arguments()
-    driver = get_driver(inp.username)
-    driver.get("https://rewards.bing.com/")
-    sleep(3)
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    randomBingSearch(driver, inp.searches)
+    inp = get_arguments()    
+    maxTries = 5
+    searchesDone = 0
+    while searchesDone<inp.searches:
+        if maxTries == 0:
+            print("Maximum tries reached. Exiting...")
+            exit()
+        try:
+            driver = get_driver(inp.username)
+        except Exception as e:
+            logging.error(f"Error in getting driver: \t {e}")
+            print(f"You might have provided wrong username.PLEASE CHECK USERNAME. Error in webdriver.")
+            exit()
+        driver.get("https://rewards.bing.com/")
+        sleep(3)
+        
+        # driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[-1])
+        searchesDone += randomBingSearch(driver, inp.searches-searchesDone)
+        driver.quit()
+        maxTries -= 1
+        print(f"Searches done: {searchesDone}")
